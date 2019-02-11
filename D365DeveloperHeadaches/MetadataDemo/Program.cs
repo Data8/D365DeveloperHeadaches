48db1140b22530db647a21886391f0b69911989d
Matt Beard <mattb@data-8.co.uk>
2019-02-11 15:02:48 +0000

    Added metadata 01

---
+++ D365DeveloperHeadaches/MetadataDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+
+namespace MetadataDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var crmSvc = new CrmServiceClient(ConfigurationManager.ConnectionStrings["CRMUGDemo"].ConnectionString);
+            var org = crmSvc.OrganizationServiceProxy;
+
+            var newRecord = new Entity("data8_metadata")
+            {
+                ["data8_thealphabet"] = "abcdefghijklmnopqrstuvwxyz"
+            };
+
+            newRecord.Id = org.Create(newRecord);
+            Console.WriteLine($"Record created ({newRecord.Id})");
+        }
+    }
+}
