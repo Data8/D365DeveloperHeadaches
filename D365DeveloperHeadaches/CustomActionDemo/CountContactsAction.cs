46f908aab2b653bfe7a9d7b36d47cd0cf776cd57
Matt Beard <mattb@data-8.co.uk>
2019-02-11 15:30:20 +0000

    Added new action

---
+++ D365DeveloperHeadaches/CustomActionDemo/CountContactsAction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace CustomActionDemo
+{
+    public class CountContactsAction : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var tracing = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            var orgFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var org = orgFactory.CreateOrganizationService(context.UserId);
+            var systemOrg = orgFactory.CreateOrganizationService(null);
+
+            var target = (EntityReference)context.InputParameters["Target"];
+            var contactQry = new QueryExpression("contact")
+            {
+                ColumnSet = new ColumnSet("firstname", "lastname"),
+                Criteria =
+                {
+                    Conditions =
+                    {
+                        new ConditionExpression("parentcustomerid", ConditionOperator.Equal, target.Id)
+                    }
+                }
+            };
+
+            var contactResults = org.RetrieveMultiple(contactQry);
+            context.OutputParameters["TotalCount"] = contactResults.Entities.Count;
+        }
+    }
+}
