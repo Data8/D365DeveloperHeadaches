1e7a803a51ab9a6e60853b4205a193786ffe7928
Matt Beard <mattb@data-8.co.uk>
2019-02-12 10:02:59 +0000

    Extra fields

--- D365DeveloperHeadaches/MetadataDemo/Program.cs
+++ D365DeveloperHeadaches/MetadataDemo/Program.cs
@@ -16,13 +16,32 @@ namespace MetadataDemo
             var crmSvc = new CrmServiceClient(ConfigurationManager.ConnectionStrings["CRMUGDemo"].ConnectionString);
             var org = crmSvc.OrganizationServiceProxy;
 
-            var newRecord = new Entity("data8_metadata")
+            var newRecord1 = new Entity("data8_metadata")
             {
+                ["data8_name"] = "Alphabet Record",
                 ["data8_thealphabet"] = "abcdefghijklmnopqrstuvwxyz"
             };
 
-            newRecord.Id = org.Create(newRecord);
-            Console.WriteLine($"Record created ({newRecord.Id})");
+            newRecord1.Id = org.Create(newRecord1);
+            Console.WriteLine($"Record created ({newRecord1.Id})");
+
+            var newRecord2 = new Entity("data8_metadata")
+            {
+                ["data8_name"] = "Magic Number Record",
+                ["data8_themagicnumber"] = 7
+            };
+
+            newRecord2.Id = org.Create(newRecord2);
+            Console.WriteLine($"Record created ({newRecord2.Id})");
+
+            var newRecord3 = new Entity("data8_metadata")
+            {
+                ["data8_name"] = "The Wrong Record",
+                ["data8_therating"] = new OptionSetValue(888880002) // Bad
+            };
+
+            newRecord3.Id = org.Create(newRecord3);
+            Console.WriteLine($"Record created ({newRecord3.Id})");
         }
     }
 }
